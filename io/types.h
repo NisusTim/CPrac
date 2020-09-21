dfa559f936098b7e07150fd07b62b385add331ef
NisusTim <nisustim@gmail.com>
2020-09-21 19:04:13 +0800

    NsTm/develop: New ipv4_sockets.

--- io/types.h
+++ io/types.h
@@ -2,10 +2,16 @@
 // pointer/address type
 //
 //
+#ifndef TYPES_H_
+#define TYPES_H_
 
-enum {
+typedef enum {
+  TRUE     = 1,
+  FALSE    = 0,
   ENABLED  = 1,
   DISABLED = 0,
   SUCCESS  = 0,
   FAILURE  = 1
-};
+} bool;
+
+#endif  // TYPES_H_
