3ce8434a0420364939f3c61cb837cfff93194279
NisusTim <nisustim@gmail.com>
2020-10-11 09:56:07 +0800

    NsTm/develop: Complete source of doubly linked list.

--- linked_list/ns_list.h
+++ linked_list/ns_list.h
@@ -187,7 +187,8 @@ static inline void list_move(struct list_head *list, struct list_head *head)
  *
  * e.g. [a, b, c, d] move b at d >>> [a, c, b, d]
  */
-static inline void list_move_tail(struct list_head *list, struct list_head *head)
+static inline void list_move_tail(struct list_head *list, 
+                                  struct list_head *head)
 {
   list_del(list);
   list_add_tail(list, head);
@@ -657,5 +658,54 @@ static inline void list_splice_tail_init(struct list_head *list,
          n = list_next_entry(pos, member);                       \
        &(pos)->member != (head);                                 \
        pos = n, n = list_next_entry(n, member);)
+
+/*
+ * list_for_each_entry_safe_from - iterate over list from current point safe
+ * against removal.
+ * @pos: the type * to use as a loop cursor.
+ * @n: another type * to use as temporary storage.
+ * @head: the head for your list.
+ * @member: the name of the list_head within the struct.
+ *
+ * Iterate over list of given type from current point, safe against removal of
+ * list entry.
+ */
+#define list_for_each_entry_safe_from(pos, n, head, member)  \
+  for (n = list_next_entry(pos, member);                     \
+       &(pos)->member != (head);                             \
+       pos = n, n = list_next_entry(n, member))
+
+/*
+ * list_for_each_entry_safe_reverse - iterate backwards over list safe against
+ * removal.
+ * @pos: the type * to use as a loop cursor.
+ * @n: another type * to use as temporary storage.
+ * @head: the head for your list.
+ * @member: the name of the list_head within the struct.
+ *
+ * Iterate backwards over list of given type, safe against removal of lit
+ * entry.
+ */
+#define list_for_each_entry_safe_reverse(pos, n, head, member)  \
+  for (pos = list_last_entry(head, typeof(*(pos)), member),     \
+         n = list_prev_entry(pos, member);                      \
+       &(pos)->member != (head);                                \
+       pos = n, n = list_prev_entry(n, member))
+
+/*
+ * list_safe_reset_next - reset a stale list_for_each_entry_safe loop>
+ * @pos: the loop cursor used in the list_for_each_entry_safe loop.
+ * @n: temporary storage used in list_for_each_entry_safe.
+ * @: the name of the list_head within the struct.
+ *
+ * list_safe_reset_next is not safe to use in general if the list may be
+ * modified concurrently (eg. the lock is dropped in the loop body).  An
+ * exception to this is if the cursor element (pos) is pinned in the list, and
+ * list_safe_reset_next is called after re-taking the lock and before 
+ * completing the current iteration of the loop body.
+ */
+#define list_safe_reset_next(pos, n, member)  \
+  n = list_next_entry(pos, member)
+
 
 #endif  /* NS_LIST_H_ */
